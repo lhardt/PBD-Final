cfd6cba13c1da2c14af91c2b6b092183e64c3bbb
Rodrigo Bervig <rodrigo_bervig@hotmail.com>
2024-08-24 15:33:03 -0300

    define base functions for adding in the database

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -1,3 +1,4 @@
 # db/__init__.py
 from .connection import get_db_client
 from .create_db import create_imoveisdb_collections
+from .insert_documents import insert_property_listing, insert_crawler_log
